8ba65666a9ed3c916a3352d1277bee70a127dcd6
jaeminjeong <21900675@handong.edu>
2023-01-11 20:54:00 +0900

    leetcode FizzBuzz 풀이

---
+++ PPS 3주차 제출파일/A074_정재민_20230111.cpp
@@ -0,0 +1,41 @@
+// pps 74번 leetcode Fizz Buzz
+
+// 2023.01.11 수
+
+//정답
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<string> fizzBuzz;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            fizzBuzz.push_back("FizzBuzz");
+        else if (i % 3 == 0)
+            fizzBuzz.push_back("Fizz");
+        else if (i % 5 == 0)
+            fizzBuzz.push_back("Buzz");
+        else
+            fizzBuzz.push_back(to_string(i));
+    }
+
+    for (int i = 0; i < fizzBuzz.size(); i++)
+    {
+        std::cout << "Random- : " << fizzBuzz[i] << std::endl;
+    }
+}
+
+// std::vector<int>::iterator iter;
+// for(iter = v.begin(); iter!= v.end(); iter++)
+// {
+//     std::cout << "Iterator : " << *iter << std::endl;
+// }
