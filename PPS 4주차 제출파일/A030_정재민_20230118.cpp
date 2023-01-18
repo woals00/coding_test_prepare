043e3b340397965524d31fbc389c3101b3f09a80
jaeminjeong <21900675@handong.edu>
2023-01-18 18:42:00 +0900

    백준 17211번 풀이

---
+++ PPS 4주차 제출파일/A030_정재민_20230118.cpp
@@ -0,0 +1,45 @@
+// pps a30번 백준 17211번
+
+// 2023.01.18 수
+
+//정답
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int N, n;
+    double m[4], h = 0, s = 0, tmp1, tmp2;
+
+    cin >> N >> n;
+    cin >> m[0] >> m[1] >> m[2] >> m[3];
+
+    if (n == 1)
+    {
+        h = m[2];
+        s = m[3];
+    }
+    else if (n == 0)
+    {
+        h = m[0];
+        s = m[1];
+    }
+
+    for (int i = 0; i < N - 1; i++)
+    {
+        tmp1 = h * m[0] + s * m[2];
+        tmp2 = s * m[3] + h * m[1];
+        h = tmp1;
+        s = tmp2;
+    }
+
+    h = h * 1000;
+    s = s * 1000;
+
+    cout << (int)h << endl;
+    cout << (int)s << endl;
+    return 0;
+}
