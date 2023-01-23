eb591365e837ea233f2e7cf002d9d713a32f9667
jaeminjeong <21900675@handong.edu>
2023-01-23 13:58:21 +0900

    leetcode 844. Backspace String Compare 풀이

--- PPS 4주차 제출파일/미해결 문제모음/A042_정재민_20230113.cpp
+++ PPS 4주차 제출파일/미해결 문제모음/A042_정재민_20230113.cpp
@@ -2,30 +2,63 @@
 
 // 2023.01.13 금
 
+//정답
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main()
-{
-    string s, t;
-    vector<char> sv;
-    vector<string> tv;
-    cin >> s;
-    // cin >> t;
-
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (s[i] == '#')
-        {
-            sv.pop_back();
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        for(int i=0;i<s.size();++i){
+            if(s[0]=='#'){
+                s.erase(0,1);
+                i=0;
+                while(true){
+                    if(s[0]=='#' and s!=""){
+                        s.erase(0,1);
+                        i=0;
+                    }
+                    else
+                        break;
+                }
+                if(s=="")
+                    break;
+            }
+            if(s[i]=='#' and i!=0){
+                s.erase(i-1,2);
+                i=i-2;
+                if(s=="")
+                    break;
+            }
         }
-        else
-        {
-            sv.push_back(s[i]);
+         for(int i=0;i<t.size();++i){
+            if(t[0]=='#'){
+                t.erase(0,1);
+                i=0;
+                while(true){
+                    if(t[0]=='#' and t!=""){
+                        t.erase(0,1);
+                        i=0;
+                    }
+                    else
+                        break;
+                }
+                if(t=="")
+                    break;
+            }
+            if(t[i]=='#' and i!=0){
+                t.erase(i-1,2);
+                i=i-2;
+                if(t=="")
+                    break;
+            }
         }
-        cout << sv[i];
+        if(s==t)
+            return true;
+        return false;
     }
-}
+};
