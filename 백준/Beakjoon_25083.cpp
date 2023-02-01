a6dc2775680d2badda72765da1c1eebbef8b634c
jaeminjeong <21900675@handong.edu>
2023-02-01 20:53:02 +0900

    Beakjoon_25083번 풀이

---
+++ 백준/Beakjoon_25083.cpp
@@ -0,0 +1,17 @@
+//백준 25083번 
+
+//2023.02.01 수
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+    }
